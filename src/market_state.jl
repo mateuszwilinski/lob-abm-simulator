d2b02497a40b7792362df402a512959b9d65cf78
mateuszwilinski <mateuszwilinski@o2.pl>
2023-12-13 11:04:57 +0100

    updated market depth function

--- src/market_state.jl
+++ src/market_state.jl
@@ -19,16 +19,10 @@ function market_depth(book::Book)
     depth = zeros(Float64, bids_size + asks_size, 2)
     for (i, p) in enumerate(keys(book.bids))
         depth[i, 1] = p
-        if length(book.bids[p]) == 0
-            println(book.bids)
-        end
-        depth[i, 2] = sum(get_size(o) for o in book.bids[p])
+        depth[i, 2] = -sum(get_size(o) for o in book.bids[p])
     end
     for (i, p) in enumerate(keys(book.asks))
         depth[i + bids_size, 1] = p
-        if length(book.asks[p]) == 0
-            println(book.asks)
-        end
         depth[i + bids_size, 2] = sum(get_size(o) for o in book.asks[p])
     end
 
