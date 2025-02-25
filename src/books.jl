4ebd069a8789ffec5677dfab32e761e33061768a
mateuszwilinski <mateuszwilinski@o2.pl>
2025-02-25 21:30:28 +0100

    returned to the previous book format

--- src/books.jl
+++ src/books.jl
@@ -1,13 +1,41 @@
 
 import DataStructures: OrderedSet
 
-Base.@kwdef mutable struct Book
-    bids::Dict{Float64, OrderedSet{LimitOrder}} = Dict()
-    asks::Dict{Float64, OrderedSet{LimitOrder}} = Dict()
-    best_bid::Float64 = NaN
-    best_ask::Float64 = NaN
-    time::Int64 = 0
-    symbol::String = ""
-    trades::Vector{Trade} = Trade[]
-    ticker_size::Union{Float64, Nothing} = nothing
+mutable struct Book
+    bids::Dict{Float64, OrderedSet{LimitOrder}}
+    asks::Dict{Float64, OrderedSet{LimitOrder}}
+    best_bid::Float64
+    best_ask::Float64
+    time::Int64
+    symbol::String
+    trades::Vector{Trade}
+    tick_size::Float64
+end
+
+"""
+    update_best_ask!(book)
+
+Updates the "best_ask" field in the book according
+to the current state.
+"""
+function update_best_ask!(book::Book)
+    if isempty(keys(book.asks))
+        book.best_ask = NaN
+    else
+        book.best_ask = minimum(keys(book.asks))
+    end
+end
+
+"""
+    update_best_bid!(book)
+
+Updates the "best_bid" field in the book according
+to the current state.
+"""
+function update_best_bid!(book::Book)
+    if isempty(keys(book.bids))
+        book.best_bid = NaN
+    else
+        book.best_bid = maximum(keys(book.bids))
+    end
 end
