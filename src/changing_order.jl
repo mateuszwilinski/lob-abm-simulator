a295e7f21a3a0cbad425ec2d11e98716191748e6
mateuszwilinski <mateuszwilinski@o2.pl>
2025-02-26 13:11:05 +0100

    restricted order size to positive values

--- src/changing_order.jl
+++ src/changing_order.jl
@@ -90,6 +90,9 @@ For the order with id equal to "order_id" in the "book" and the "agent"'s orders
 change order's size into "new_size".
 """
 function modify_order!(order_id::Int64, new_size::Int64, book::Book, agent::Agent)
+    if new_size <= 0
+        throw(error("Order size must be positive."))
+    end
     if agent.orders[order_id].is_bid
         delete!(book.bids[agent.orders[order_id].price], agent.orders[order_id])
         push!(book.bids[agent.orders[order_id].price], agent.orders[order_id])
@@ -97,7 +100,7 @@ function modify_order!(order_id::Int64, new_size::Int64, book::Book, agent::Agen
         delete!(book.asks[agent.orders[order_id].price], agent.orders[order_id])
         push!(book.asks[agent.orders[order_id].price], agent.orders[order_id])
     end
-    agent.orders[order_id].size[] = new_size  # TODO: zero should not be allowed
+    agent.orders[order_id].size[] = new_size
 end
 
 """
