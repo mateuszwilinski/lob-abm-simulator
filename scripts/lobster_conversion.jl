543a55d52b6b7fec721c801f1124ba9ca74b4bb9
mateuszwilinski <mateuszwilinski@o2.pl>
2025-03-27 13:40:18 +0100

    allowed for custom number of levels for lobster snapshots format

--- scripts/lobster_conversion.jl
+++ scripts/lobster_conversion.jl
@@ -18,16 +18,18 @@ function main()
     
     events = DataFrame(CSV.File(events_input, delim=',', header=false))
     snapshots = DataFrame(CSV.File(snapshots_input, delim=',', header=false))
+    levels = Int64(size(snapshots)[2] / 4)
 
     # Add the header
-    rename!(events, [:time, :type, :id, :size, :price, :dir, :agent, :cross_order, :cross_agent])
-    rename!(snapshots, [
-        :ask_1_price, :ask_1_vol, :bid_1_price, :bid_1_vol,
-        :ask_2_price, :ask_2_vol, :bid_2_price, :bid_2_vol,
-        :ask_3_price, :ask_3_vol, :bid_3_price, :bid_3_vol,
-        :ask_4_price, :ask_4_vol, :bid_4_price, :bid_4_vol,
-        :ask_5_price, :ask_5_vol, :bid_5_price, :bid_5_vol
-        ])
+    rename!(events, ["time", "type", "id", "size", "price", "dir", "agent", "cross_order", "cross_agent"])
+    snapshots_columns = String[]
+    for i in 1:levels
+        push!(snapshots_columns, "ask_$(i)_price")
+        push!(snapshots_columns, "ask_$(i)_vol")
+        push!(snapshots_columns, "bid_$(i)_price")
+        push!(snapshots_columns, "bid_$(i)_vol")
+    end
+    rename!(snapshots, snapshots_columns)
 
     # Select only the LOBSTER columns
     events = select(events, [:time, :type, :id, :size, :price, :dir])
