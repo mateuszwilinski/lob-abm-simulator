df1d27f9920c2c57eeca75ce9455e38e6f537d1f
mateuszwilinski <mateuszwilinski@o2.pl>
2025-09-30 11:34:19 +0200

    extended the standard budget

--- experiments/configs/net_abm_params.jl
+++ experiments/configs/net_abm_params.jl
@@ -1,7 +1,7 @@
 
 agents_params = Dict(
     "default" => Dict( # info_rate, limit_rate, market_rate, cancel_rate, sigma, size, size_sigma, budget
-                    "NetTrader" => (1000.0, 5000.0, 20000.0, 40000.0, 2.0, 5, 1.5, Budget(10, 1000.0))
+                    "NetTrader" => (1000.0, 5000.0, 20000.0, 40000.0, 2.0, 5, 1.5, Budget(50, 5000.0))
                     ),
     "budget" => Dict(
                     "NetTrader" => (1000.0, 5000.0, 20000.0, 40000.0, 2.0, 5, 1.5, Budget(2, 200.0))
